c098c1181a7bbdd6270d7f18d03b8293b302ad03
WJX2015 <550481089@qq.com>
2017-03-11 20:41:06 +0800

    优化代码，提高效率

--- app/src/main/java/com/example/lenovo_g50_70/listview/MyAdapter.java
+++ app/src/main/java/com/example/lenovo_g50_70/listview/MyAdapter.java
@@ -24,11 +24,24 @@ public class MyAdapter extends ArrayAdapter<Girl> {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         Girl girl=getItem(position);
-        View view= LayoutInflater.from(getContext()).inflate(resourceId,parent,false);
-        ImageView iv= (ImageView) view.findViewById(R.id.iv);
-        TextView tv= (TextView) view.findViewById(R.id.tv);
-        iv.setImageResource(girl.getImage());
-        tv.setText(girl.getName());
+        View view;
+        ViewHolder viewHolder;
+        if(convertView==null){//converView用于缓存已加载的布局，此处不重复加载布局
+            view=LayoutInflater.from(getContext()).inflate(resourceId,parent,false);
+            viewHolder=new ViewHolder();
+            viewHolder.girlImage= (ImageView) view.findViewById(R.id.iv);
+            viewHolder.girlName= (TextView) view.findViewById(R.id.tv);
+            view.setTag(viewHolder);//将ViewHolder存储在View中
+        }else{
+            view=convertView;
+            viewHolder= (ViewHolder) view.getTag();//重新获取ViewHolder
+        }
+        viewHolder.girlImage.setImageResource(girl.getImage());
+        viewHolder.girlName.setText(girl.getName());
         return view;
+    }
+    class ViewHolder{//新增内部类用于对控件的实例缓存
+        ImageView girlImage;
+        TextView girlName;
     }
 }
